93aa83c0d507ebcc5ccef736a86ce51e263c615e
Sebastian Galeano <sebascarp9@gmail.com>
2024-11-03 10:14:32 -0300

    Header component update

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,11 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { ArrowLeft, Plus } from 'lucide-react'
 
-export default function Header({ view }) {
+type HeaderProps = {
+  view: 'landing' | 'create' | 'edit'
+}
+
+export default function Header({ view }: HeaderProps) {
   const router = useRouter()
 
   return (
