5c62bc0df651882adddb69f9376e8144f220b3fa
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2025-01-21 13:41:58 +0000

    Image to view of project list to developers added

--- src/app/edit-projects/[id]/page.tsx
+++ src/app/edit-projects/[id]/page.tsx
@@ -1,10 +1,12 @@
 import EditProjectForm from '@/components/EditProjectForm'
-
+import Header from '@/components/Header'
 export default function EditProjectPage({ params }: { params: { id: string } }) {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-6">Editar Proyecto</h1>
+    <main className="min-h-screen bg-gray-100">
+      <Header view="edit" />
+    <section className="container mx-auto px-4 py-8">
       <EditProjectForm projectId={params.id} />
-    </div>
+    </section>
+    </main>
   )
 }
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,22 +1,42 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { MoreVertical, Edit2, Trash2, User } from 'lucide-react'
 import type { ProjectCardProps } from '@/types/components/index'
+import { Developers } from '@/types/db'
 
 export default function ProjectCard({ project, onEdit, onDelete }: ProjectCardProps) {
   const [menuOpen, setMenuOpen] = useState(false)
-
   const toggleMenu = () => setMenuOpen(!menuOpen)
+  const [developers, setDevelopers] = useState<Developers[]>([])
+  useEffect(() => {
+    const fetchDevelopers = async () => {
+      try {
+        const response = await fetch('/api/developers');
+        const data: Developers[] = await response.json();
+        setDevelopers(data);
+      } catch (error) {
+        console.error('Error al cargar los Developers:', error);
+      }
+    };
+    fetchDevelopers(); 
+  }, []);
 
+  const assignedDeveloper = developers.find(dev => dev.name === project.assignedDev);
+  
   return (
     <section className="bg-white rounded-lg shadow-md p-6 relative">
       <h2 className="text-xl font-semibold text-gray-800 mb-4">{project.name}</h2>
       <p className="text-gray-600 mb-4">{project.description}</p>
       <section className="flex items-center mb-4">
+      {/* Si se encuentra el desarrollador, mostramos su imagen */}
+      {assignedDeveloper && assignedDeveloper.img? (
+        <img src={assignedDeveloper.img} alt={assignedDeveloper.name} className="w-8 h-8 rounded-full mr-2" />
+      ) : (
         <User size={20} className="text-gray-500 mr-2" />
-        <span className="text-gray-600">{project.assignedDev}</span>
-      </section>
+      )}
+      <span className="text-gray-600">{assignedDeveloper ? assignedDeveloper.name : 'Desarrollador no asignado'}</span>
+    </section>
       <p className="text-sm text-gray-500">Project Manager: {project.projectManager}</p>
       <p className="text-sm text-gray-500 mt-2">
         Estado: <span className={project.status === 'enabled' ? 'text-green-500' : 'text-red-500'}>
